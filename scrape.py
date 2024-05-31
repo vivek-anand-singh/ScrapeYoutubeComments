4b08194511d57a435a77a62f81049872537060a9
Vivek Anand Singh <145670267+vivek-anand-singh@users.noreply.github.com>
2024-05-31 19:43:48 +0530

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,15 +1,1 @@
-from youtube_comment_downloader import YoutubeCommentDownloader
 
-# Initialize the downloader
-downloader = YoutubeCommentDownloader()
-
-# Get comments for the specified YouTube video ID
-youtube_id = input("Enter the YouTube video ID: ")
-comments = downloader.get_comments(youtube_id)
-
-# Print each comment text
-
-store_comments = []
-for comment in comments:
-    store_comments.append(comment.text)
-    print(comment.text)
